f365ce8e4471c3b7263a7fc3b95fcbd92f5e65fa
Samuel Demirdjian <a@enov.ws>
2013-06-20 13:52:09 +0300

    TestCase Reflection

---
+++ application/tests/aurora/reflectiontest.php
@@ -0,0 +1,21 @@
+<?php
+
+class Aurora_ReflectionTest extends Unittest_TestCase
+{
+	public function provider_cname() {
+		return array(
+			array(new Model_Event, 'set_start', 'DateTime'),
+			array(new Model_Event, 'set_end', 'DateTime'),
+			array(new Model_Event, 'set_title', NULL),
+		);
+	}
+	/**
+	 * @dataProvider provider_cname
+	 */
+	public function test_typehint($model, $setter, $expected) {
+		$this->assertEquals(
+		  Aurora_Reflection::typehint($model, $setter), $expected
+		);
+	}
+
+}
