3c83aee795884fa96f65f2114be33b59c89d8fef
Samuel Demirdjian <a@enov.ws>
2013-07-08 13:26:38 +0300

    test: Aurora_Au

---
+++ application/tests/aurora/autest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Test case for Aurora_Core class
+ *
+ * @group Aurora
+ * @group Aurora.Core
+ */
+class Aurora_AuTest extends Unittest_TestCase
+{
+	public function test_au() {
+		$this->assertTrue(Au::type() instanceof Aurora_Type);
+		$this->assertTrue(Au::db() instanceof Aurora_Database);
+		$this->assertTrue(Au::prop() instanceof Aurora_Property);
+		$this->assertTrue(Au::rflx() instanceof Aurora_Reflection);
+		$this->assertTrue(Au::std() instanceof Aurora_StdClass);
+		$this->assertTrue(Au::hook() instanceof Aurora_Hook);
+		$this->assertTrue(Au::type() instanceof Aurora_Type);
+	}
+}
