fec16365fbb542890b80e7132e7d5aa03b41865b
Samuel Demirdjian <a@enov.ws>
2013-07-26 15:31:02 +0300

    Autoload test

---
+++ application/tests/aurora/autoloadtest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Test case for Aurora_Core class
+ *
+ * @group Aurora
+ * @group Aurora.Core
+ */
+class Aurora_AutoloadTest extends Unittest_TestCase
+{
+	/**
+	 * provider for test_load_aurora and test_load_collection
+	 */
+	public function provider_autoload() {
+		return array(
+			array('event'),
+		);
+	}
+	/**
+	 * @dataProvider provider_autoload
+	 */
+	public function test_load_aurora($cname) {
+		$au = Au::factory($cname, 'Aurora');
+		$this->assertTrue(Au::type()->is_aurora($au));
+	}
+	/**
+	 * @dataProvider provider_autoload
+	 */
+	public function test_load_collection($cname) {
+		$col = Au::factory($cname, 'Collection');
+		$this->assertTrue(Au::type()->is_collection($col));
+	}
+}
