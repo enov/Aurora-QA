feff8270adf5fa058bf5ebba3294b16494827327
Samuel Demirdjian <a@enov.ws>
2013-09-02 12:03:07 +0300

    Enable RESTful route, as it is now disabled in the module

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -114,6 +114,11 @@ Kohana::modules(array(
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
+
+// Aurora RESTful API
+Route::set('rest-api', array('Aurora_Route', 'route'));
+
+// Default Route
 Route::set('default', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
 		'controller' => 'welcome',
